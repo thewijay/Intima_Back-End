4c7318a41cee6f317471fe1446c9756fd9f1a0a1
Pubudu Wijesundara <neo17641@gmail.com>
2025-02-05 22:53:20 +0530

    Initialize Django REST Framework project

---
+++ text/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+# menu items
+class Item(models.Model):
+    name = models.CharField(max_length=250)
+    price = models.IntegerField()
+    description = models.TextField()
+
+    def __str__(self):
+        return f'{self.name} | {self.price}'
